438e43b01d1a6d549fa5586e8919c77276cc8623
ivan <ivanstrel87@gmail.com>
2019-03-11 23:16:08 +0300

    HWCommonCase.r and HW_NatSelect.r refactoring was performed

--- functions/HWCommonCase.r
+++ functions/HWCommonCase.r
@@ -1,38 +1,45 @@
-HWCommonCase<-function(PopN=200,PopSize=200){
-	#Function for demonstration of hardy weinberg equilibrium
-	P<-seq(0,1,length.out=PopN)
-	Q<-1-P
-	PQ<-cbind(P,Q)
-	Freq<-function(x,PopSize){
-		#Internal function for new frequences culculation
-		#x -- frequence of p or (A) allele
-		A<-rmultinom(1,PopSize,prob=c((x[1]**2),(2*x[1]*x[2]),(x[2]**2)))
-		A<-as.numeric(A)
-		A<-A/PopSize
-		return(A)
-	}
-	#Calculation of Hardy-Weinberg frequences for each
-	#starting conditions
-	FreqVect<-apply(PQ,1,Freq,PopSize=PopSize)
-	FreqVect<-t(FreqVect)
-	colnames(FreqVect)<-c("AA","Aa","aa")
-	
-	#Calculate teoretical (expected) genotypes frequences
-	AA_line<-seq(0,1,length.out=100)**2
-	Aa_line<-2*seq(0,1,length.out=100)*(1-seq(0,1,length.out=100))
-	aa_line<-(1-seq(0,1,length.out=100))**2
-	
-	#Plot expected frequences as lines
-	plot(NULL,xlim=c(0,1),ylim=c(0,1),xlab="Частоты гамет f(A) или 1/f(a)",ylab="Частоты генотипов",main="Распределение частот по Харди и Вайнбергу",font=2,font.lab=2)
-	points(seq(0,1,length.out=100),AA_line,t="l",lwd=2,col=2)
-	points(seq(0,1,length.out=100),Aa_line,t="l",lwd=2,col="forestgreen")
-	points(seq(0,1,length.out=100),aa_line,t="l",lwd=2,col="blue")
-	
-	#Plot simulated values as points
-	points(P,FreqVect[,1],col="red")
-	points(P,FreqVect[,2],col="forestgreen")
-	points(P,FreqVect[,3],col="blue")
-	#Add the legend to the plot
-	legend("top",legend=c("АА","Аа","аа"),pch=c(1,1,1),lwd=c(2,2,2),col=c("red","forestgreen","blue"),ins=0.1)
+hw_common_case <- function(pop_n = 200, pop_size = 200) {
+    #Function for demonstration of hardy weinberg equilibrium
+    p <- seq(0, 1, length.out = pop_n)
+    q <- 1 - p
+    pq <- cbind(p, q)
+    freq <- function(x,pop_size){
+        #Internal function for new frequences culculation
+        #x -- frequence of p or (A = "a_big") allele
+        a_big <- rmultinom(1, pop_size, prob = c((x[1] ** 2), (2 * x[1] * x[2]), (x[2] ** 2)))
+        a_big <- as.numeric(a_big)
+        a_big <- a_big / pop_size
+        return(a_big)
+    }
+
+    #Calculation of Hardy-Weinberg frequences for each
+    #starting conditions
+    freq_vect <- apply(pq, 1, freq, pop_size = pop_size)
+    freq_vect <- t(freq_vect)
+    colnames(freq_vect) <- c("AA", "Aa", "aa")
+
+    #Calculate teoretical (expected) genotypes frequences
+    AA_line <- seq(0, 1, length.out = 100) ** 2
+    Aa_line <- 2 * seq(0, 1, length.out = 100) * (1 - seq(0, 1, length.out = 100))
+    aa_line <- (1 - seq(0, 1, length.out = 100)) ** 2
+
+    #Plot expected frequences as lines
+    plot(NULL, xlim = c(0, 1), ylim = c(0, 1),
+         xlab = "Частоты гамет f(A) или 1/f(a)",
+         ylab = "Частоты генотипов",
+         main = "Распределение частот по Харди и Вайнбергу",
+         font = 2, font.lab = 2, cex.lab = 1.5, cex.axis = 1.5, cex.main = 1.5)
+    points(seq(0, 1, length.out = 100), AA_line, t = "l", lwd = 3, col = 2)
+    points(seq(0, 1, length.out = 100), Aa_line, t = "l", lwd = 3, col = "forestgreen")
+    points(seq(0, 1, length.out = 100), aa_line, t = "l", lwd = 3, col = "blue")
+
+    #Plot simulated values as points
+    points(p, freq_vect[, 1], cex = 1.5, col = "red")
+    points(p, freq_vect[, 2], cex = 1.5, col = "forestgreen")
+    points(p, freq_vect[, 3], cex = 1.5, col = "blue")
+
+    #Add the legend to the plot
+    legend("top", legend = c("АА", "Аа", "аа"),
+           pch = c(1, 1, 1), lwd = c(2, 2, 2),
+           col = c("red", "forestgreen", "blue"), ins = 0.1)
 }
-	
--- functions/HW_NatSelect.r
+++ functions/HW_NatSelect.r
@@ -1,47 +1,54 @@
-HW_NatSelect<-function(NGen=50,fP=0.5,PopSize=10000,fAA=0.85,fAa=0.9,faa=0.8){
-	#Function for visualization of genoripes proportions under an
-	#influence of selection and gene drift
-	#fP -- frequence of A allele
-	#fQ -- frequence of a allele
-	FreqInit<-fP
-	#calculate fQ
-	fQ<-1-fP
-	#set initial 
-	Res_tab<-rmultinom(1,PopSize,prob=c((fP**2),(2*fQ*fP),(fQ**2)))
-	Res_tab<-as.numeric(Res_tab)
-	#Generate genotypes sequences
-	for (i in c(2:NGen)){
-		fQ<-1-fP
-		#Generation of new frequences with multinomial distribution of size 3
-		Pop<-rmultinom(1,PopSize,prob=c((fP**2)*fAA,(2*fQ*fP)*fAa,(fQ**2)*faa))
-		Pop<-as.numeric(Pop)
-		fP<-(Pop[1]*2+Pop[2])/(PopSize*2)
-		Res_tab<-rbind(Res_tab,Pop)
-	}
-	#Plot results
-	par(mar=c(7,4,1,1))
-		#Add empty plot
-	plot(NULL,ylim=c(1,PopSize),xlim=c(1,NGen),xlab="Поколений", ylab="Количество генотипов",font.lab=2,font=2)
-	AA<-Res_tab[,1]
-	Aa<-Res_tab[,2]
-	aa<-Res_tab[,3]
-		#Create polygons for each genotype (by count)
-	polygon(c(1,1:NGen,NGen),c(0,aa,0),col="lightblue")
-	Aa<-Aa+aa
-	polygon(c(1:NGen,NGen:1),c(Aa,rev(aa)),col="olivedrab2")
-	AA<-AA+Aa
-	polygon(c(1:NGen,NGen:1),c(AA,rev(Aa)),col="brown1")
-		#Add lines of starting genotypes counts
-	abline(h=aa[1],lty=2,lwd=2)
-	abline(h=Aa[1],lty=2,lwd=2)
-	
-	#Add legend
-	par(mar=c(0,4,1,1),new=TRUE)
-	plot(NULL,xlim=c(1,1),ylim=c(1,1),xaxt="n",yaxt="n",bty="n",xlab="",ylab="")
-	legend("bottom",legend=c("AA","Aa","aa"),fill=rev(c("lightblue","olivedrab2","brown1")),bty="n",horiz=TRUE,cex=1.5,xpd=TRUE)
-	
-	#Add computation of Hardy-Weinberg
-	Tab<-Res_tab[c(1,NGen),]
-	#chisq.test(Tab)
+hw_nat_select <- function(n_gen = 50, fp = 0.5, pop_size = 10000, fAA = 0.85, fAa = 0.9, faa =0.8) {
+    #Function for visualization of genoripes proportions under an
+    #influence of selection and gene drift
+    #fp -- frequence of A allele
+    #fq -- frequence of a allele
+    #freq_init <- fp
+    #calculate fq
+    fq <- 1 - fp
+    #set initial 
+    res_tab <- rmultinom(1, pop_size, prob = c((fp ** 2), (2 * fq * fp), (fq ** 2)))
+    res_tab <- as.numeric(res_tab)
+    #Generate genotypes sequences
+    for (i in c(2:n_gen)){
+        fq <- 1 - fp
+        #Generation of new frequences with multinomial distribution of size 3
+        pop <- rmultinom(1, pop_size, prob = c((fp ** 2) * fAA, (2 * fq * fp) * fAa, (fq ** 2) * faa))
+        pop <- as.numeric(pop)
+        fp <- (pop[1] * 2 + pop[2]) / (pop_size * 2)
+        res_tab <- rbind(res_tab, pop)
+    }
+    #Plot results
+    par(mar = c(7, 4, 1, 1))
+        #Add empty plot
+    plot(NULL, ylim = c(1, pop_size), xlim = c(1, n_gen),
+         xlab = "Поколений", ylab = "Количество генотипов",
+         font.lab = 2, font = 2,
+         cex.lab = 1.5, cex.axis = 1.5)
+    AA <- res_tab[, 1]
+    Aa <- res_tab[, 2]
+    aa <- res_tab[, 3]
+
+    #Create polygons for each genotype (by count)
+    polygon(c(1, 1:n_gen, n_gen), c(0, aa, 0), col = "lightblue")
+    Aa <- Aa + aa
+    polygon(c(1:n_gen, n_gen:1), c(Aa, rev(aa)), col = "olivedrab2")
+    AA <- AA + Aa
+    polygon(c(1:n_gen, n_gen:1), c(AA, rev(Aa)), col = "brown1")
+
+    #Add lines of starting genotypes counts
+    abline(h = aa[1], lty = 2, lwd = 2)
+    abline(h = Aa[1], lty = 2, lwd = 2)
+
+    #Add legend
+    par(mar = c(0, 4, 1, 1), new = TRUE)
+    plot(NULL, xlim = c(1, 1), ylim = c(1, 1),
+         xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
+    legend("bottom", legend = c("AA", "Aa", "aa"),
+           fill = rev(c("lightblue", "olivedrab2", "brown1")),
+           bty = "n", horiz = TRUE, cex = 1.5, xpd = TRUE)
+
+    #Add computation of Hardy-Weinberg
+    tab <- res_tab[c(1, n_gen), ]
+    #chisq.test(Tab)
 }
-			
